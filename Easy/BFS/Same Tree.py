5d557da8a5d6f5f8ed32432829f8068bba9eb393
VladG <gricenkovlad007@gmail.com>
2025-03-10 22:26:26 +0300

    Solved Easy problem BFS

--- Easy/BFS/Same Tree.py
+++ Easy/BFS/Same Tree.py
@@ -11,15 +11,26 @@ class TreeNode:
 
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        q_q = deque()
-        q_p = deque()
-        q_p += p
-        q_q += q
-        while q_q and q_p:
-            for _ in range(len(q_q)):
-                node = q_q.popleft()
-                node2 = q_p.popleft()
-                if node != node2:
+            queue = deque()
+            queue.append((p, q))  # Добавляем корни обоих деревьев
+            while queue:
+                node1, node2 = queue.popleft()  # Извлекаем узлы из очереди
+                
+                # Если оба узла None, пропускаем
+                if not node1 and not node2:
+                    continue
+                
+                # Если один из узлов None, а другой нет, деревья не идентичны
+                if not node1 or not node2:
                     return False
-                i
-        
+                
+                # Если значения узлов не равны, деревья не идентичны
+                if node1.val != node2.val:
+                    return False
+                
+                # Добавляем левых и правых потомков в очередь
+                queue.append((node1.left, node2.left))
+                queue.append((node1.right, node2.right))
+            return True
+s = Solution()
+print(s.isSameTree(q=TreeNode([1,2,3]), p=TreeNode([1,2,3])))
