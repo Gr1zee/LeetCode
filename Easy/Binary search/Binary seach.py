a804edf731851adf6cc014070d996f0f4682dd52
vladg-note <gricenkovlad007@gmail.com>
2025-11-25 15:11:23 +0300

    Solved binary search

---
+++ Easy/Binary search/Binary seach.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = len(nums)
+        left = 0
+        right = len(nums) - 1
+        while right >= left:
+            mid = left + (right - left) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+
+
+print(1 // 2)
